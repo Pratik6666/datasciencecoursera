f15b275aa73cb1c4a8e09d318169ae355a5c3fcf
Pratik6666 <ppchowdhury6@gmail.com>
2020-09-13 12:20:33 +0530

    reading CSV (excel files)

--- practice set.R
+++ practice set.R
@@ -1,11 +1,3 @@
-x=c("Pratik",26,200101112,"MBA Fulltime")
-names(x)
-names(x)=c("name of student","age","roll","course")
-names(x)
-table(x)
-y=list(a=1,b=2,c=3,a=4)
-y
-m=matrix(c("a","b","c","d"),nrow=2,ncol=2)
-m
-dimnames(m)=list(c("row1","row2"),c("col1","col2"))
-m
+student=read.csv("input_for_R.csv",TRUE)
+student
+class(student)
