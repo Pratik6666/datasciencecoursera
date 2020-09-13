e1544132886a67cf8d249b819df227ab454fc5f0
Pratik6666 <ppchowdhury6@gmail.com>
2020-09-13 11:24:58 +0530

    NA and NaN operations
    and table operation too!

--- practice set.R
+++ practice set.R
@@ -1,9 +1,5 @@
-x=factor(c("yes","no","yes","yes","no","rocket","science"),levels = c("no","yes","rocket","science"))
-x
-table(x)
-unclass(x)
-y=c(1,2,3,4,5)
-y=factor(c(1,2,3,4,5,2,3,4,2,4,2,2,3))
-y
-table(y)
-unclass(y)
+x=c(1,2,NA,3,4)
+is.na(x)
+y=c(1,2,NA,3,4,5,NaN,NA,0,NaN)
+table(is.nan(y))
+table(is.na(y))
