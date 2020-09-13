2a7da177175492cd9c178417ba6e1dd7adff5b74
Pratik6666 <ppchowdhury6@gmail.com>
2020-09-13 11:50:02 +0530

    Naming Data types vectors list matrix

--- practice set.R
+++ practice set.R
@@ -1,6 +1,11 @@
-x=data.frame(foo=1:4,bar=c(T,T,F,F))
-x
-y=data.frame(test1=c("a","b","c","d"),test2=c("a1","b1","c1","d1"))
+x=c("Pratik",26,200101112,"MBA Fulltime")
+names(x)
+names(x)=c("name of student","age","roll","course")
+names(x)
+table(x)
+y=list(a=1,b=2,c=3,a=4)
 y
-nrow(x)
-ncol(y)
+m=matrix(c("a","b","c","d"),nrow=2,ncol=2)
+m
+dimnames(m)=list(c("row1","row2"),c("col1","col2"))
+m
