19b5db9d675531037f2bfc98998d6ec246f52dd4
Pratik6666 <ppchowdhury6@gmail.com>
2020-09-14 20:33:38 +0530

    If else

--- practice set.R
+++ practice set.R
@@ -1,5 +1,8 @@
-x=c("a","b","c","d","e","f")
-x[1]
-x[x>"a"]
-LV = x > "b"
-LV
+x = -03.000000000000000000000000001
+if (x<0) {
+  print("This is a negative number")
+} else if (x>0) {
+  print("This is a positive number")
+} else {
+  print("the number is 0")
+}
